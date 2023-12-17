da208cdce3d2d36280b0cfa5ba7676b5dcecc5b8
Azimont457 <kktotov457@gmail.com>
2023-12-18 04:57:02 +0500

    Saving result

---
+++ my-app/src/components/About/index.jsx
@@ -0,0 +1,26 @@
+import './style.scss'
+import Background from '../../assets/images/bag2.png'
+function About() {
+    return (
+        <div className="about-body">
+            <div className="about">
+                <div className="bag">
+                    <img src={Background} width={'100%'} height={'100%'} />
+                </div>
+                <div className="about-us">
+                    <p id="title">
+                        О нас
+                    </p>
+                    <br />
+                    <p id="subtitle">
+                        PeroTravel - Первый онлайн-сервис по бронированию экскурсий без очередей и операторов. <br /> <br />
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. <br /> <br />
+                        Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
+                    </p>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default About;
---
+++ my-app/src/components/Types/index.jsx
@@ -0,0 +1,14 @@
+import './style.scss'
+function Types() {
+    return ( 
+        <div className="types-about">
+            <div className="types">
+                <div className="top">
+                    <p id="title">Виды экскурсий</p>
+                </div>
+            </div>
+        </div>
+     );
+}
+
+export default Types;
