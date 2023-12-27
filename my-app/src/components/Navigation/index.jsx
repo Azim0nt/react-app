fa86fb566b253e11f81149cb37b1d714a53e2034
Azim0nt <kktotov457@gmail.com>
2023-12-27 14:04:22 +0500

    Saving result

--- my-app/src/components/Navigation/index.jsx
+++ my-app/src/components/Navigation/index.jsx
@@ -1,33 +1,36 @@
 import './style.scss';
 import Logo from '../../assets/icons/лого.png'
-import { Outlet, Link, NavLink} from "react-router-dom"
+import { Outlet, Link, NavLink } from "react-router-dom"
 
 function Navigation() {
-    return ( 
+    return (
         <div id='body'>
             <nav id="navigation-wrapper">
                 <div className="left">
                     <NavLink to={'/'}>
-                    <img src={Logo} alt="Logo" />
+                        <img src={Logo} alt="Logo" />
                     </NavLink>
                 </div>
                 <div className="right">
-                        <ul>
-                            <li><NavLink to={'/main'}>Главная</NavLink></li>
-                            <li><NavLink to={'/excursion'}>Экскурсии</NavLink></li>
-                            <li><NavLink to={'/about'}>О нас</NavLink></li>
-                            <li><NavLink to={'/types'}>Виды</NavLink></li>
-                            <li><NavLink to={'/gallery'}>Галерея</NavLink></li>
-                            <li><NavLink to={'/feedback'}>Отзывы</NavLink></li>
-                            <li><NavLink to={'/leave-feedback'}>Оставить вопрос</NavLink></li>
-                        </ul>
+                    <ul>
+                        <li><NavLink to={'/main'}>Главная</NavLink></li>
+                        <li><NavLink to={'/excursion'}>Экскурсии</NavLink></li>
+                        <li><NavLink to={'/about'}>О нас</NavLink></li>
+                        <li><NavLink to={'/types'}>Виды</NavLink></li>
+                        <li><NavLink to={'/gallery'}>Галерея</NavLink></li>
+                        <li><NavLink to={'/feedback'}>Отзывы</NavLink></li>
+                        <li><NavLink to={'/leave-feedback'}>Оставить вопрос</NavLink></li>
+                    </ul>
                 </div>
             </nav>
+            <p id="mobil-nav">
+                <img src={Logo} alt="" /> 
+            </p>
             <div id="outlet">
                 <Outlet />
             </div>
         </div>
-     );
+    );
 }
 
 export default Navigation;
