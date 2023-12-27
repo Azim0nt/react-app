fb2ae7400a159990810af6f0809dfc428734ea89
Azim0nt <kktotov457@gmail.com>
2023-12-27 13:41:15 +0500

    Saving result

--- my-app/src/components/Feedback/index.jsx
+++ my-app/src/components/Feedback/index.jsx
@@ -1,7 +1,10 @@
 import './style.scss'
-import feedbackImage from '../../assets/images/отзывы.png'
+import feedbackImage from '../../assets/images/отзывы.png';
+import Marquee from 'react-fast-marquee';
+import Photo1 from '../../assets/images/photo1.png';
+import Photo2 from '../../assets/images/photo2.png';
 function Feedback() {
-    return ( 
+    return (
         <div className='feedback-body'>
             <div className="feedback">
                 <div className="top">
@@ -13,11 +16,39 @@ function Feedback() {
                     </p>
                 </div>
                 <div className="bottom">
-                    <img src={feedbackImage} width={1530} height={500} />
+                    <Marquee direction='right' className='marquee' autoFill={true}>
+
+                        <div className="f-1">
+                            <div className='left'> <img src={Photo1} alt="" /></div>
+                            <div className='right'><p className="title">Иван Иванов, 25 лет</p><p className="about-they">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing <br />
+                                elit, sed do eiusmod tempor incididunt ut labore et  <br />
+                                dolore magna aliqua. Ut enim ad minim veniam, quis nostrud  <br />
+                                exercitation ullamco laboris nisi ut aliquip ex ea  <br />
+                                commodo consequat. Duis aute irure dolor in reprehenderit in <br />
+                                voluptate velit esse cillum dolore eu fugiat nulla pariatur. <br />
+                                Excepteur sint occaecat cupidatat non proident, sunt in culpa <br />
+                                qui officia deserunt mollit anim id est laborum.
+                            </p></div>
+                        </div>
+                        <div className="f-2">
+                            <div className='left'> <img src={Photo2} alt="" /></div>
+                            <div className='right'><p className="title">Иван Иванов, 25 лет</p><p className="about-they">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing <br />
+                                elit, sed do eiusmod tempor incididunt ut labore et  <br />
+                                dolore magna aliqua. Ut enim ad minim veniam, quis nostrud  <br />
+                                exercitation ullamco laboris nisi ut aliquip ex ea  <br />
+                                commodo consequat. Duis aute irure dolor in reprehenderit in <br />
+                                voluptate velit esse cillum dolore eu fugiat nulla pariatur. <br />
+                                Excepteur sint occaecat cupidatat non proident, sunt in culpa <br />
+                                qui officia deserunt mollit anim id est laborum. <br />
+                            </p></div>
+                        </div>
+                    </Marquee>
                 </div>
             </div>
         </div>
-     );
+    );
 }
 
 export default Feedback;
--- my-app/src/components/Gallery/index.jsx
+++ my-app/src/components/Gallery/index.jsx
@@ -1,5 +1,12 @@
 import './style.scss'
-import imageGallery from '../../assets/images/галерея.png'
+import Image1 from '../../assets/images/img-1.png'
+import Image2 from '../../assets/images/img-2.png'
+import Image3 from '../../assets/images/img-3.png'
+import Image4 from '../../assets/images/img-4.png'
+import Image5 from '../../assets/images/img-5.png'
+import Image6 from '../../assets/images/img-6.png'
+import Image7 from '../../assets/images/img-7.png'
+import Image8 from '../../assets/images/img-8.png'
 
 function Gallery() {
     return (
@@ -7,7 +14,28 @@ function Gallery() {
             <div className="gallery">
                 <div className="top"><p className="left">Популярные экскурсии</p> <p className="right">Смотреть все</p></div>
                 <div className="bottom">
-                    <img src={imageGallery} width={'100%'} />
+                    <div className="left">
+                        <div className="top"></div>
+                        <div className="bottom-l">
+                            <div className="left-b">
+
+                            </div>
+                            <div className="right-b">
+                                <div className="top-b"></div>
+                                <div className="bottom-b"></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="middle">
+
+                    </div>
+                    <div className="right">
+                        <div className="top-r">
+                            <div className="left-r"></div>
+                            <div className="right-r"></div>
+                        </div>
+                        <div className="bottom-r"></div>
+                    </div>
                 </div>
             </div>
 
