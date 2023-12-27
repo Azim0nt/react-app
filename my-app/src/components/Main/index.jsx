8203a60438036580e4e35ffdc446e6c7d8fa75e7
Azim0nt <kktotov457@gmail.com>
2023-12-27 12:04:46 +0500

    Saving result

--- my-app/src/components/Main/index.jsx
+++ my-app/src/components/Main/index.jsx
@@ -3,18 +3,22 @@ import Title from '../../assets/images/заголовок.png'
 import Video from '../../assets/images/видео.png'
 import Excursion from '../Excursion';
 function Main() {
-    return ( 
+    return (
         <div className="body-v">
-             <div className='body-main'>
-            <div className="main">
-                <img src={Title} alt="" />
-                <button id='btn'>К экскурсиям</button>
+            <div className='body-main'>
+                <div className="main">
+                    <div className="text">
+                        <p className="title">ПУТЕШЕСТВУЙ</p>
+                        <p className="litle-text">вместе с</p>
+                        <p className="subtitle">PERO TRAVEL</p>
+                    </div>
+                    <button id='btn'>К экскурсиям</button>
+                </div>
             </div>
+            <img src={Video} alt="Video" id='video' />
         </div>
-            <img src={Video} alt="Video" id='video'/>
-        </div>
-       
-     );
+
+    );
 }
 
 export default Main;
