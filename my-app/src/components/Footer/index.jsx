4b34f20891fb1c7549859249af99e7023f6cad85
Azim0nt <kktotov457@gmail.com>
2024-01-05 15:09:51 +0500

    Saving result

--- my-app/src/components/Footer/index.jsx
+++ my-app/src/components/Footer/index.jsx
@@ -1,19 +1,24 @@
 import './style.scss';
 import Logo from '../../assets/images/logo_color 1.png'
 import Icons from '../../assets/images/соц сети.png'
-import {NavLink} from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 function Footer() {
     return (
         <div className="footer-body">
             <div className="footer">
                 <NavLink to={"/"}>
-
-                <img src={Logo} width={80} height={114} />
+                    <img src={Logo} width={80} height={114} />
                 </NavLink>
                 <div className="text-1">
-                    <p>Главная</p>
-                    <p>Экскурсии</p>
-                    <p>Личный кабинет</p>
+                    <NavLink to={'/'}>
+                        <p>Главная</p>
+                    </NavLink>
+                    <NavLink to={'/excursion'}>
+                        <p>Экскурсии</p>
+                    </NavLink>
+                    <NavLink to={'/about'}>
+                        <p>О нас</p>
+                    </NavLink>
                 </div>
                 <div className="text-2">
                     <p>+7 964 944 18 74</p>
